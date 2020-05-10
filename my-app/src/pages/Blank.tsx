54911be856179c0e955b5b03b87f52058f8b8b19
Marcos Sa <marcoshgsa@gmail.com>
2020-05-10 04:41:06 -0300

    Start navigation
    command done):

---
+++ my-app/src/pages/Blank.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Blank extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>TAMO GRANDE</h1>
+            </div>
+        )
+    }
+  }
+  
+  
+  export default Blank;
